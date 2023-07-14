e9f6ccb7a49cb1842038dfd58a2eb3b5057bc0be
Christian Schwarz <cmrs@cmrs.io>
2023-07-14 20:54:02 +0200

    improve benchs

--- bench/lib.rs
+++ bench/lib.rs
@@ -1,4 +1,5 @@
 #![cfg(feature = "unstable")]
+#![feature(string_extend_from_within)]
 #![feature(test)]
 extern crate test;
 
@@ -25,31 +26,11 @@ fn empty_context(b: &mut test::Bencher) {
     });
 }
 
-#[bench]
-fn large_string_many_regex_matches(b: &mut test::Bencher) {
-    const COUNT: usize = 10000;
-    const MATCHES: usize = 1000;
-
-    b.iter(|| {
-        let ss = StringSinkHandle::new();
-        ContextBuilder::default()
-            .push_str(&int_sequence_newline_separated(COUNT), 1)
-            .add_op(create_op_regex_lines())
-            .add_op(create_op_regex("^[0-9]{1,3}$", RegexOptions::default()).unwrap())
-            .add_op(create_op_string_sink(&ss))
-            .run()
-            .unwrap();
-        assert_eq!(
-            ss.get_data().unwrap().as_slice(),
-            &int_sequence_strings(MATCHES)
-        );
-    });
-}
-
-const LEN: usize = 1000;
+const LEN: usize = 2000;
 
 #[bench]
 fn plain_string_sink(b: &mut test::Bencher) {
+    let res = int_sequence_strings(LEN);
     b.iter(|| {
         let ss = StringSinkHandle::new();
         ContextBuilder::default()
@@ -57,12 +38,52 @@ fn plain_string_sink(b: &mut test::Bencher) {
             .add_op(create_op_string_sink(&ss))
             .run()
             .unwrap();
-        assert_eq!(ss.get_data().unwrap().as_slice(), int_sequence_strings(LEN));
+        assert_eq!(ss.get_data().unwrap().as_slice(), res);
+    });
+}
+
+#[bench]
+fn regex_lines(b: &mut test::Bencher) {
+    const COUNT: usize = 1000;
+    let input = int_sequence_newline_separated(COUNT);
+    let res = int_sequence_strings(COUNT);
+    b.iter(|| {
+        let ss = StringSinkHandle::new();
+        ContextBuilder::default()
+            .push_str(&input, 1)
+            .add_op(create_op_regex_lines())
+            .add_op(create_op_string_sink(&ss))
+            .run()
+            .unwrap();
+        assert_eq!(ss.get_data().unwrap().as_slice(), res);
+    });
+}
+
+#[bench]
+fn regex_lines_plus_drop_uneven(b: &mut test::Bencher) {
+    const COUNT: usize = 1000;
+    let input = int_sequence_newline_separated(COUNT);
+    let res: Vec<String> = int_sequence_strings(COUNT)
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, v)| if i % 2 == 0 { Some(v) } else { None })
+        .collect();
+    b.iter(|| {
+        let ss = StringSinkHandle::new();
+        ContextBuilder::default()
+            .push_str(&input, 1)
+            .add_op(create_op_regex_lines())
+            .add_op(create_op_regex("^.*[02468]$", RegexOptions::default()).unwrap())
+            .add_op(create_op_string_sink(&ss))
+            .run()
+            .unwrap();
+        assert_eq!(ss.get_data().unwrap().as_slice(), res);
     });
 }
 
 #[bench]
 fn dummy_format(b: &mut test::Bencher) {
+    let res = int_sequence_strings(LEN);
     b.iter(|| {
         let ss = StringSinkHandle::new();
         ContextBuilder::default()
@@ -71,6 +92,54 @@ fn dummy_format(b: &mut test::Bencher) {
             .add_op(create_op_string_sink(&ss))
             .run()
             .unwrap();
-        assert_eq!(ss.get_data().unwrap().as_slice(), int_sequence_strings(LEN));
+        assert_eq!(ss.get_data().unwrap().as_slice(), res);
+    });
+}
+
+#[bench]
+fn format_twice(b: &mut test::Bencher) {
+    let mut res = int_sequence_strings(LEN);
+    for v in res.iter_mut() {
+        v.extend_from_within(0..);
+    }
+    b.iter(|| {
+        let ss = StringSinkHandle::new();
+        ContextBuilder::default()
+            .add_op(create_op_seq(0, LEN as i64, 1).unwrap())
+            .add_op(create_op_format("{}{}".as_bytes().as_bstr()).unwrap())
+            .add_op(create_op_string_sink(&ss))
+            .run()
+            .unwrap();
+        assert_eq!(ss.get_data().unwrap().as_slice(), res);
+    });
+}
+
+#[bench]
+fn regex_drop_uneven_into_format_twice(b: &mut test::Bencher) {
+    const COUNT: usize = 1000;
+    let input = int_sequence_newline_separated(COUNT);
+    let res: Vec<String> = int_sequence_strings(COUNT)
+        .into_iter()
+        .enumerate()
+        .filter_map(|(i, mut v)| {
+            if i % 2 == 0 {
+                v.extend_from_within(0..);
+                Some(v)
+            } else {
+                None
+            }
+        })
+        .collect();
+    b.iter(|| {
+        let ss = StringSinkHandle::new();
+        ContextBuilder::default()
+            .push_str(&input, 1)
+            .add_op(create_op_regex_lines())
+            .add_op(create_op_regex("^.*[02468]$", RegexOptions::default()).unwrap())
+            .add_op(create_op_format("{}{}".as_bytes().as_bstr()).unwrap())
+            .add_op(create_op_string_sink(&ss))
+            .run()
+            .unwrap();
+        assert_eq!(ss.get_data().unwrap().as_slice(), res);
     });
 }
