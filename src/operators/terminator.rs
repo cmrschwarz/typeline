f4f33d1f690546127ec70368f2d715fbe92fc959
Christian Schwarz <cmrs@cmrs.io>
2023-11-23 16:45:08 +0100

    add delayed deletion to terminator

--- src/operators/terminator.rs
+++ src/operators/terminator.rs
@@ -7,6 +7,7 @@ use super::transform::{TransformData, TransformId, TransformState};
 
 pub struct TfTerminator {
     actor_id: ActorId,
+    delayed_deletion_row_count: usize,
 }
 
 pub fn setup_tf_terminator(
@@ -17,19 +18,28 @@ pub fn setup_tf_terminator(
         .action_buffer;
     TransformData::Terminator(TfTerminator {
         actor_id: cb.add_actor(),
+        delayed_deletion_row_count: 0,
     })
 }
 
 pub fn handle_tf_terminator(
     sess: &mut JobData,
     tf_id: TransformId,
-    tt: &TfTerminator,
+    tft: &mut TfTerminator,
 ) {
     let (batch_size, input_done) = sess.tf_mgr.claim_all(tf_id);
     let tf = &sess.tf_mgr.transforms[tf_id];
     let ab = &mut sess.match_set_mgr.match_sets[tf.match_set_id].action_buffer;
-    ab.begin_action_group(tt.actor_id);
-    ab.push_action(FieldActionKind::Drop, 0, batch_size);
+    ab.begin_action_group(tft.actor_id);
+    let rows_to_drop;
+    if tf.successor.is_some() {
+        rows_to_drop = tft.delayed_deletion_row_count;
+        tft.delayed_deletion_row_count = batch_size;
+    } else {
+        rows_to_drop = tft.delayed_deletion_row_count + batch_size;
+        tft.delayed_deletion_row_count = 0;
+    }
+    ab.push_action(FieldActionKind::Drop, 0, rows_to_drop);
     ab.end_action_group();
     if input_done {
         sess.unlink_transform(tf_id, batch_size);
