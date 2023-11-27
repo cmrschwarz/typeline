779830c52ee2b9483da96973fa79df77f23e5c16
Christian Schwarz <cmrs@cmrs.io>
2023-11-27 21:14:40 +0100

    add setup method to Extension

--- core/src/extension.rs
+++ core/src/extension.rs
@@ -2,33 +2,43 @@ use crate::{
     cli::ParsedCliArgument,
     operators::{errors::OperatorCreationError, operator::OperatorData},
     options::session_options::SessionOptions,
-    utils::small_box::SmallBox,
 };
 
+#[derive(Default)]
 pub struct ExtensionRegistry {
-    pub extensions: Vec<SmallBox<dyn Extension, 8>>,
+    pub extensions: Vec<Box<dyn Extension>>,
+}
+
+impl ExtensionRegistry {
+    pub fn setup(&mut self) {
+        struct DummyExt;
+        impl Extension for DummyExt {
+            fn try_match_cli_argument(
+                &self,
+                _ctx_opts: &SessionOptions,
+                _arg: &ParsedCliArgument,
+                _args: &[Vec<u8>],
+            ) -> Result<Option<OperatorData>, OperatorCreationError>
+            {
+                unimplemented!()
+            }
+        }
+        let mut dummy_ext: Box<dyn Extension> = Box::new(DummyExt);
+        for i in 0..self.extensions.len() {
+            let mut ext =
+                std::mem::replace(&mut self.extensions[i], dummy_ext);
+            ext.setup(self);
+            dummy_ext = std::mem::replace(&mut self.extensions[i], ext);
+        }
+    }
 }
 
 pub trait Extension: Send + Sync {
+    fn setup(&mut self, registry: &ExtensionRegistry) {}
     fn try_match_cli_argument(
         &self,
         ctx_opts: &SessionOptions,
         arg: &ParsedCliArgument,
         args: &[Vec<u8>],
     ) -> Result<Option<OperatorData>, OperatorCreationError>;
-}
-
-impl Default for ExtensionRegistry {
-    fn default() -> Self {
-        #[allow(unused_mut)]
-        let mut res = Self {
-            extensions: Vec::new(),
-        };
-
-        #[cfg(feature = "sqlite")]
-        res.extensions
-            .push(SmallBox::new(ext_sqlite::SqliteExtension::new()));
-
-        res
-    }
 }
