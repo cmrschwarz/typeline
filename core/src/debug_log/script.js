4b8eb1e5b3116795492f126edcdfcfc0ff27c90d
Christian Schwarz <cmrs@cmrs.io>
2024-06-27 15:43:10 +0200

    gather nested highlight tags on collapsable

--- core/src/debug_log/script.js
+++ core/src/debug_log/script.js
@@ -1,9 +1,10 @@
 function toggle_collapsable(
     elements_tag, collapsed_elements_tag, display_style = "flex")
 {
-    let elements = document.querySelectorAll(`[collapse_tag=${elements_tag}]`);
-    let collapsed_elements =
-        document.querySelectorAll(`[collapse_tag=${collapsed_elements_tag}]`);
+    const elements =
+        document.querySelectorAll(`[collapse_tag="[${elements_tag}]"]`);
+    const collapsed_elements = document.querySelectorAll(
+        `[collapse_tag="[${collapsed_elements_tag}]"]`);
     if (elements[0].style.display == "none") {
         for (const elem of elements) {
             elem.style.display = display_style;
@@ -16,15 +17,49 @@ function toggle_collapsable(
         for (const elem of elements) {
             elem.style.display = "none";
         }
-        for (const collapsed_elem of collapsed_elements) {
+        for (var i = 0; i < collapsed_elements.length; i++) {
+            const elem = elements[i];
+            const collapsed_elem = collapsed_elements[i];
+            gather_nested_highlight_tags(elem, collapsed_elem);
             collapsed_elem.style.display = "flex";
         }
     }
 }
 
+function add_tags(tag_set, tag_str)
+{
+    for (const tag of (tag_str ?? "").split(/\s+/)) {
+        tag_set.add(tag);
+    }
+}
+
+function gather_nested_highlight_tags(element, collapsed_element)
+{
+    if (collapsed_element.hasAttribute("nested_highlight_tags_gathered")) {
+        return;
+    }
+    let highlight_tag_set = new Set();
+
+    add_tags(
+        highlight_tag_set, collapsed_element.getAttribute("highlight_tag"));
+
+    for (elem of element.querySelectorAll(`[highlight_tag]`)) {
+        add_tags(highlight_tag_set, elem.getAttribute("highlight_tag"));
+    }
+
+    let final_highlight_tag = "";
+    for (const tag of highlight_tag_set) {
+        final_highlight_tag += tag;
+    }
+
+    collapsed_element.setAttribute("highlight_tag", final_highlight_tag.trim());
+    collapsed_element.setAttribute("nested_highlight_tags_gathered", "");
+}
+
 function highlight(tag)
 {
-    let highlightables = document.querySelectorAll(`[highlight_tag=${tag}]`);
+    const highlightables =
+        document.querySelectorAll(`[highlight_tag*="[${tag}]"]`);
     for (const elem of highlightables) {
         elem.classList.add('highlighted')
     }
@@ -32,7 +67,8 @@ function highlight(tag)
 
 function unhighlight(tag)
 {
-    let highlightables = document.querySelectorAll(`[highlight_tag=${tag}]`);
+    const highlightables =
+        document.querySelectorAll(`[highlight_tag*="[${tag}]"]`);
     for (const elem of highlightables) {
         elem.classList.remove('highlighted')
     }
