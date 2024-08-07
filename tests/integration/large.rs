30c8c77f07379dbee303f5330552c63cf5a5417e
Christian Schwarz <cmrs@cmrs.io>
2024-08-07 20:23:38 +0200

    add broken count test

---
+++ tests/integration/large.rs
@@ -0,0 +1,20 @@
+use scr::{
+    operators::{
+        count::create_op_count, regex::create_op_regex,
+        sequence::create_op_seq,
+    },
+    options::context_builder::ContextBuilder,
+    scr_error::ScrError,
+};
+
+#[test]
+fn multibatch_count() -> Result<(), ScrError> {
+    let res = ContextBuilder::without_exts()
+        .add_op(create_op_seq(0, 100, 1)?)
+        .set_batch_size(10)?
+        .add_op(create_op_regex("12")?)
+        .add_op(create_op_count())
+        .run_collect_as::<i64>()?;
+    assert_eq!(res, [100]);
+    Ok(())
+}
