805b4c83ec54f1a516d0b4f7ffc6959b0ddc9d6f
Christian Schwarz <cmrs@cmrs.io>
2025-01-03 17:13:21 +0100

    add crates.io and docs.rs badges

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 //! [![github]](https://github.com/cmrschwarz/typeline)&ensp;
 //! [![github-build]](https://github.com/cmrschwarz/typeline/actions/workflows/ci.yml)&ensp;
+//! [![crates-io]](https://crates.io/crates/typeline)&ensp;
+//! [![msrv]](https://crates.io/crates/typeline)&ensp;
+//! [![docs-rs]](https://docs.rs/typeline)&ensp;
 //!
 //! [github]: https://img.shields.io/badge/cmrschwarz/typeline-8da0cb?&labelColor=555555&logo=github
 //! [github-build]: https://github.com/cmrschwarz/typeline/actions/workflows/ci.yml/badge.svg
-//! [github-build-shields]: https://img.shields.io/github/actions/workflow/status/cmrschwarz/typeline/ci.yml?branch=main&logo=github
+//! [crates-io]: https://img.shields.io/crates/v/typeline.svg?logo=rust
+//! [msrv]: https://img.shields.io/crates/msrv/typeline?logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-typeline-66c2a5?logo=docs.rs
 //!
 //! An efficient, type-safe pipeline processing language.
 //!
