587de3611550fd7de719f21d5db7c8ad8d8926f4
Christian Schwarz <cmrs@cmrs.io>
2025-01-29 16:21:10 +0100

    simplify cast

--- core/src/operators/compute/operations/cast.rs
+++ core/src/operators/compute/operations/cast.rs
@@ -1,10 +1,9 @@
-use std::arch::x86_64::{_mm256_cmpeq_epi64, _mm256_set1_pd};
-
 use std::arch::x86_64::{
     __m256d, __m256i, _mm256_add_epi64, _mm256_and_si256, _mm256_andnot_si256,
-    _mm256_blendv_epi8, _mm256_castpd_si256, _mm256_cmpgt_epi64,
-    _mm256_or_si256, _mm256_set1_epi64x, _mm256_slli_epi64, _mm256_sllv_epi64,
-    _mm256_srli_epi64, _mm256_srlv_epi64, _mm256_sub_epi64, _mm256_xor_si256,
+    _mm256_blendv_epi8, _mm256_castpd_si256, _mm256_cmpeq_epi64,
+    _mm256_cmpgt_epi64, _mm256_max_epi32, _mm256_or_si256, _mm256_set1_epi64x,
+    _mm256_set1_pd, _mm256_slli_epi64, _mm256_sllv_epi64, _mm256_srli_epi64,
+    _mm256_srlv_epi64, _mm256_sub_epi64, _mm256_xor_si256,
 };
 
 // roughly based on an idea from https://stackoverflow.com/a/68176624/7204912
@@ -12,14 +11,16 @@ use std::arch::x86_64::{
 pub unsafe fn f64_to_i64(v: __m256d) -> (__m256i, __m256i) {
     unsafe {
         // Constants
-        let kneg1 = _mm256_set1_epi64x(-1);
-        let k0 = _mm256_set1_epi64x(0);
-        let k1 = _mm256_set1_epi64x(1);
-        let k51 = _mm256_set1_epi64x(51);
-        let mask_7ff = _mm256_set1_epi64x(0x7FF);
-        let mask_3ff = _mm256_set1_epi64x(0x3FF);
-        let mask_52bits = _mm256_set1_epi64x((1 << 52) - 1);
-        let i64_max = _mm256_set1_epi64x(i64::MAX);
+        let c_minus_one = _mm256_set1_epi64x(-1);
+        let c_zero = _mm256_set1_epi64x(0);
+        let c_one = _mm256_set1_epi64x(1);
+        let c_51 = _mm256_set1_epi64x(51);
+        let c_63 = _mm256_set1_epi64x(63);
+        let c_7ff = _mm256_set1_epi64x(0x7FF);
+        let c_3ff = _mm256_set1_epi64x(0x3FF);
+        let c_mantissa_one_bit = _mm256_slli_epi64(c_one, 52);
+        let c_52bit_mask = _mm256_sub_epi64(c_mantissa_one_bit, c_one);
+        let c_i64_max = _mm256_set1_epi64x(i64::MAX);
 
         #[allow(clippy::cast_precision_loss)]
         let i64_min_pd = _mm256_set1_pd(i64::MIN as f64);
@@ -27,44 +28,47 @@ pub unsafe fn f64_to_i64(v: __m256d) -> (__m256i, __m256i) {
         // Bitcast to integer registers
         let vi = _mm256_castpd_si256(v);
 
-        // Extract exponents (bits 52-62)
-        let biased_exp = _mm256_and_si256(_mm256_srli_epi64(vi, 52), mask_7ff);
-        let exp = _mm256_sub_epi64(biased_exp, mask_3ff);
+        // Extract exponents (bits 52-62), subtract 0x3ff bias
+        let biased_exp = _mm256_and_si256(_mm256_srli_epi64(vi, 52), c_7ff);
+        let exp = _mm256_sub_epi64(biased_exp, c_3ff);
 
         // Check if exponent < 63
-        let k63 = _mm256_set1_epi64x(63);
-        let in_range = _mm256_cmpgt_epi64(k63, exp);
+        let in_range = _mm256_cmpgt_epi64(c_63, exp);
 
         // Calculate shift amounts
-        let exp_n = _mm256_sub_epi64(k51, exp);
-        let exp_p = _mm256_sub_epi64(exp, k51);
-        let shift_mnt =
-            _mm256_blendv_epi8(k0, exp_n, _mm256_cmpgt_epi64(exp_n, k0));
-        let shift_int =
-            _mm256_blendv_epi8(k0, exp_p, _mm256_cmpgt_epi64(exp_p, k0));
+        let shift_pos_raw = _mm256_sub_epi64(exp, c_51);
+        let shift_pos = _mm256_max_epi32(shift_pos_raw, c_zero);
+        let shift_neg_raw = _mm256_sub_epi64(c_51, exp);
+        let shift_neg = _mm256_max_epi32(shift_neg_raw, c_zero);
 
-        // Extract mantissa and add implicit bit
-        let mantissa = _mm256_and_si256(vi, mask_52bits);
-        let implicit_bit =
-            _mm256_or_si256(mantissa, _mm256_slli_epi64(k1, 52));
+        // Extract mantissa and add implicit one bit
+        let mantissa_raw = _mm256_and_si256(vi, c_52bit_mask);
+        let mantissa = _mm256_or_si256(mantissa_raw, c_mantissa_one_bit);
         let int52 =
-            _mm256_srlv_epi64(implicit_bit, _mm256_add_epi64(shift_mnt, k1));
+            _mm256_srlv_epi64(mantissa, _mm256_add_epi64(shift_neg, c_one));
 
         // Shift to create integer part
-        let shifted = _mm256_sllv_epi64(int52, shift_int);
+        let shifted = _mm256_sllv_epi64(int52, shift_pos);
 
         // Restore lost bit (conditional shift)
-        let shift_restore = _mm256_sub_epi64(shift_int, k1);
-        let restore_bits =
-            _mm256_sllv_epi64(_mm256_and_si256(mantissa, k1), shift_restore);
+        let shift_restore = _mm256_sub_epi64(shift_pos, c_one);
+        let restore_bits = _mm256_sllv_epi64(
+            _mm256_and_si256(mantissa_raw, c_one),
+            shift_restore,
+        );
         let restored = _mm256_or_si256(shifted, restore_bits);
 
         // Broadcast sign bit
-        let sign_mask =
-            _mm256_andnot_si256(_mm256_cmpgt_epi64(vi, kneg1), kneg1);
+        let sign_mask = _mm256_andnot_si256(
+            _mm256_cmpgt_epi64(vi, c_minus_one),
+            c_minus_one,
+        );
+
+        // get the 64 bit integer limit based on the sign
+        // example in 8 bit: 0x7F - 0xFF = 0x10; 0x7F - 0 = 0x7F
+        let limit = _mm256_sub_epi64(c_i64_max, sign_mask);
 
         // Select between converted value and limit
-        let limit = _mm256_sub_epi64(i64_max, sign_mask);
         let magnitude = _mm256_blendv_epi8(limit, restored, in_range);
 
         // Apply two's complement for negative values
@@ -78,7 +82,10 @@ pub unsafe fn f64_to_i64(v: __m256d) -> (__m256i, __m256i) {
 
         (
             _mm256_sub_epi64(flipped, sign_mask),
-            _mm256_xor_si256(_mm256_or_si256(is_neg_2_pow63, in_range), kneg1),
+            _mm256_xor_si256(
+                _mm256_or_si256(is_neg_2_pow63, in_range),
+                c_minus_one,
+            ),
         )
     }
 }
