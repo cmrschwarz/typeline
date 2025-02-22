1c27db413b341f3dab56272cff29c985c92eda72
Christian Schwarz <cmrs@cmrs.io>
2025-02-22 15:27:05 +0100

    fix crates io / docs references

--- crates/indexland/src/lib.rs
+++ crates/indexland/src/lib.rs
@@ -1,14 +1,14 @@
 //! [![github]](https://github.com/cmrschwarz/typeline/tree/main/crates/indexland)&ensp;
 //! [![github-build]](https://github.com/cmrschwarz/typeline/actions/workflows/ci.yml)&ensp;
-//! [![crates-io]](https://crates.io/crates/typeline)&ensp;
-//! [![msrv]](https://crates.io/crates/typeline)&ensp;
-//! [![docs-rs]](https://docs.rs/typeline)&ensp;
+//! [![crates-io]](https://crates.io/crates/indexland)&ensp;
+//! [![msrv]](https://crates.io/crates/indexland)&ensp;
+//! [![docs-rs]](https://docs.rs/indexland)&ensp;
 //!
 //! [github]: https://img.shields.io/badge/cmrschwarz/typeline-8da0cb?&labelColor=555555&logo=github
 //! [github-build]: https://github.com/cmrschwarz/typeline/actions/workflows/ci.yml/badge.svg
-//! [crates-io]: https://img.shields.io/crates/v/typeline.svg?logo=rust
-//! [msrv]: https://img.shields.io/crates/msrv/typeline?logo=rust
-//! [docs-rs]: https://img.shields.io/badge/docs.rs-typeline-66c2a5?logo=docs.rs
+//! [crates-io]: https://img.shields.io/crates/v/indexland.svg?logo=rust
+//! [msrv]: https://img.shields.io/crates/msrv/indexland?logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-indexland-66c2a5?logo=docs.rs
 //!
 //! Collections based on newtype indices for increased type safety and self
 //! documenting code.
